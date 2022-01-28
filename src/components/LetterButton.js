5cfb4aa58fdc2e387dd2b519beffdf6ff584042c
oncomouse <oncomouse@gmail.com>
2022-01-28 08:47:45 -0600

    Start of a letter button component

--- src/components/LetterButton.js
+++ src/components/LetterButton.js
@@ -1,5 +1,12 @@
+import PropTypes from 'prop-types';
+import useStore from '../state';
+
 const LetterButton = (props) => {
-  return <button />;
+  const addLetter = useStore.getStore().addLetter;
+  return <button onClick={addLetter(props.letter)}>{props.letter}</button>;
+};
+LetterButton.propTypes = {
+  letter: PropTypes.string.isRequired,
 };
 
 export default LetterButton;
