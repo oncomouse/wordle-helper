e8d4015e6ff1650edf2772e99777e597f3c01883
oncomouse <oncomouse@gmail.com>
2022-01-30 11:58:56 -0600

    Coverage for Color tests

--- src/types/Color.spec.js
+++ src/types/Color.spec.js
@@ -1,5 +1,14 @@
-import Color from './Color'
+import Color, { ColorType } from './Color'
+import PropTypes from 'prop-types'
 
+const original = console.error
+beforeEach(() => {
+  console.error = jest.fn()
+})
+
+afterEach(() => {
+  console.error = original
+})
 describe('Testing Color', () => {
   test('Colors should ID as themselves', () => {
     const a = Color.Green('a')
@@ -15,5 +24,34 @@ describe('Testing Color', () => {
   test('Color.White should not update', () => {
     const a = Color.White
     expect(Color.White.is(Color.White.update(a))).toBe(true)
+  })
+  test('Color.White should return "" when .get() is called', () => {
+    expect(Color.White.get()).toBe('')
+  })
+  test('Color.Yellow should return contents when .get() is calledc', () => {
+    const contents = 'a'
+    expect(Color.Yellow(contents).get()).toBe(contents)
+  })
+  test('Color.Green should return contents when .get() is calledc', () => {
+    const contents = 'a'
+    expect(Color.Green(contents).get()).toBe(contents)
+  })
+  test('Color.Grey should return contents when .get() is calledc', () => {
+    const contents = 'a'
+    expect(Color.Grey(contents).get()).toBe(contents)
+  })
+  test('It should validate PropTypes', () => {
+    PropTypes.checkPropTypes({
+      a: ColorType
+    }, {
+      a: Color.Yellow('x')
+    }, 'prop', 'MyComponent')
+    expect(console.error).toBeCalledTimes(0)
+    PropTypes.checkPropTypes({
+      a: ColorType
+    }, {
+      a: 'foo'
+    }, 'prop', 'MyComponent')
+    expect(console.error).toBeCalled()
   })
 })
